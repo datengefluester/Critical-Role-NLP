b82873d639112b8dd6e44fad14cc11ba4fc8bfe8
datengefluester <github_hp@posteo.de>
2021-06-16 15:03:53 +0200

    test

--- scripts/machine_learning.R
+++ scripts/machine_learning.R
@@ -464,6 +464,7 @@ saveRDS(naive_final_model, "./output/models/naive_bayes.rds")
 
 
 # XGBoost TO BE EDITED ----------------------------------------------------
+#lol
 
 xgb_spec <- boost_tree(
   trees = 1000, 
