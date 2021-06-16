e7c9a4dc55e1ea66bbf915757d9bed71f01f5941
datengefluester <github_hp@posteo.de>
2021-06-16 17:34:21 +0000

    run everything up to Naive Bayes to see if code generally works

--- scripts/machine_learning.R
+++ scripts/machine_learning.R
@@ -36,7 +36,7 @@ data <- data %>%
   filter(!grepl("AND", actor_guest))
 
 # only keep relevant variables for machine learning
-data <- data %>% select(actor_guest, episode, arc, turn_number, segment, 
+data <- data %>% select(actor_guest, episode, arc, segment, 
                         text, time_in_sec, words_per_minute, rp_combat)
 
 # transform rp_combat to be more intuitive
@@ -52,7 +52,7 @@ doParallel::registerDoParallel(cores = detectCores())
 
 
 # local
-data <- data %>% slice(1:10000)
+# data <- data %>% slice(1:10000)
 
 
 # Splitting into Training, Testing and Folds ------------------------------
@@ -73,9 +73,9 @@ folds <- vfold_cv(train, strata = actor_guest)
 
 
 # Recipe ------------------------------------------------------------------
-cr_recipe <- recipe(actor_guest ~ text +time_in_sec + words_per_minute + arc + segment + combat , data = train) %>% 
+cr_recipe <- recipe(actor_guest ~ text +time_in_sec + words_per_minute + segment + combat, data = train) %>% 
   step_downsample(actor_guest) %>%        # deal with class imbalance
-  step_dummy(arc, segment) %>%            # convert factor variables into multiple dummy variables
+  step_dummy(segment) %>%            # convert factor variables into multiple dummy variables
   step_textfeature(text) %>%              # extract text features
   step_zv(all_predictors()) %>%           # remove everything with zero variance
   step_normalize(time_in_sec, words_per_minute, contains("textfeature"))  %>%       # normalize numeric predictors
@@ -89,10 +89,12 @@ cr_juiced <- juice(cr_prep)   # get pre-processed data
 
 
 # Random Forrest ----------------------------------------------------------
+
+set.seed(19820629)
 # define it model
 rf_spec <- rand_forest(
   mtry = tune(),        # number of predictors randomly sampled
-  trees = 1000,         # numbers trees fitted
+  trees = tune(),         # numbers trees fitted
   min_n = tune()        # minimum number of data points in a node
 ) %>%
   set_mode("classification") %>%
@@ -115,8 +117,8 @@ rf_res <- tune_grid(
 rf_res %>%
   collect_metrics() %>%
   filter(.metric == "roc_auc") %>%
-  select(mean, min_n, mtry) %>%
-  pivot_longer(min_n:mtry,
+  select(mean, min_n, mtry, trees) %>%
+  pivot_longer(min_n:trees,
                values_to = "value",
                names_to = "parameter"
   ) %>%
@@ -130,8 +132,9 @@ rf_res %>%
 
 # new grid based on previous results
 rf_grid <- grid_regular(
-  min_n(range = c(15, 30)),
-  mtry(range = c(2, 10)),
+  min_n(range = c(30, 40)),
+  mtry(range = c(1, 4)),
+  trees(range = c(1000,2000)),
   levels = 1
 )
 
@@ -143,16 +146,7 @@ rf_regular <- tune_grid(
 )
 
 
-# Select Best Model 
-rf_regular %>%
-  collect_metrics() %>%
-  filter(.metric == "roc_auc") %>%
-  mutate(min_n = factor(min_n)) %>%
-  ggplot(aes(mtry, mean, color = min_n)) +
-  geom_line(alpha = 0.5, size = 1.5) +
-  geom_point() +
-  labs(y = "AUC")
-
+# Select Best Model
 rf_best_auc <- select_best(rf_regular, "roc_auc")
 
 
@@ -265,7 +259,7 @@ fit <- svm_last_fit %>%
   pivot_wider(names_from = .metric, values_from = .estimate) %>% 
   mutate(model = "svm") %>% 
   bind_rows(fit)
-  
+
 # extract final model and save it
 svm_final_model <- svm_last_fit$.workflow[[1]]
 
@@ -464,6 +458,7 @@ saveRDS(naive_final_model, "./output/models/naive_bayes.rds")
 
 
 # XGBoost TO BE EDITED ----------------------------------------------------
+#lol
 
 xgb_spec <- boost_tree(
   trees = 1000, 
@@ -533,6 +528,3 @@ fit <- xgb_last_fit %>%
   pivot_wider(names_from = .metric, values_from = .estimate) %>% 
   mutate(model = "xgb_boost") %>% 
   bind_rows(fit)
-
-
-
